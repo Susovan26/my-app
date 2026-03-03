16a2f208fb8bd1e8bd269cf3371cb7130dd5ad79
Susovan26 <susovanenterprise.tech@gmail.com>
2026-03-03 21:54:26 +0530

    update website design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from "./Components/NavBar";
 import Home from "./Components/Home";
 import About from "./Components/About";
